5a98c0c2c7afe00eb4bbb5a7cd21776817e0a43a
andy <andy@sammalmaa.fi>
2024-12-14 23:49:11 +0200

    log: make tag order consistent

--- renderer/log/log_renderer.go
+++ renderer/log/log_renderer.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"os"
 	"path"
+	"sort"
 	"strings"
 	"text/template"
 )
@@ -79,6 +80,8 @@ func (r *LogRenderer) buildTags(t map[string]string) string {
 			tags = append(tags, fmt.Sprintf("#%s", key))
 		}
 	}
+
+	sort.Strings(tags)
 
 	return strings.Join(tags, " ")
 }
